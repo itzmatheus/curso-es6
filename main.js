6e40d5f6bf37754d982bb1a80f45bc4fa86c73de
Matheus Leite <matheus.leite@servicenet.com.br>
2019-09-28 21:31:49 -0300

    Criacao de classes pai e filha, invocando metodos a parti do html

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-class TodoList {
-    // Metodo executado toda vez que a classe for instanciada.
-    constructor() {
+class List{
 
+    // Metodo executado toda vez que a classe for instanciada.
+    constructor(){
+        this.data = [];
     }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data)
+    }
+
 }
+
+class TodoList extends List {
+
+    constructor(){
+        // Chamando o construtor da classe pai (List) no construtor da classe filha. 
+        super();
+        this.usuario = 'Matheus';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+
+}
+
+var MinhaList = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaList.add('Novo todo');
+}
+
+MinhaList.mostraUsuario();
